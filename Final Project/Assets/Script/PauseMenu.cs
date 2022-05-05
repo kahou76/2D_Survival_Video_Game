724b835b0464f56d279d250d5984cfa21e3f89db
SnaraC <90677197+SnaraC@users.noreply.github.com>
2022-05-04 22:26:34 -0700

    New edit with pause/resume

---
+++ Final Project/Assets/Script/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    
+        void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPause = true;
+        }
+
+        void LoadMenu()
+        {
+            Debug.Log("Loading menu...");
+        }
+    }
+}
