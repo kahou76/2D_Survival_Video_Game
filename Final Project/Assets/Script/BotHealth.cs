f52778b4fe2c22c3e1509d7da65e823ec53a2194
SnaraC <90677197+SnaraC@users.noreply.github.com>
2022-06-01 20:51:37 -0700

    adding scoreboard

--- Final Project/Assets/Script/BotHealth.cs
+++ Final Project/Assets/Script/BotHealth.cs
@@ -10,8 +10,6 @@ public class BotHealth : MonoBehaviour
     public int MAX_HEALTH = 100;
     // public BloodBar bloodBar;
 
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,31 +20,6 @@ public class BotHealth : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     health -= 20;
-        //     if (health <= 0)
-        //     {
-        //         Debug.Log("Lol, you died. Press F to pay respect!");
-        //     }
-        // }
-
-        // if (health <= 0 && Input.GetKeyDown(KeyCode.F))
-        // {
-        //     Debug.Log("RESPECT");
-        // }
-
-
-        // if(Input.GetKeyDown(KeyCode.R)){
-        //     //Heal(10);
-        // }
-
-        // if(Input.GetKeyDown(KeyCode.K)){
-        //     // Damage(10);
-        // }
-    }
 
     public void SetHealth(int maxHealth, int health)
     {
@@ -69,10 +42,6 @@ public class BotHealth : MonoBehaviour
         }
         this.health -= amount;
 
-        // bloodBar.SetHealth(health);
-
-        // BotHealth.SetHealth(MAX_HEALTH,this.health);
-
         StartCoroutine(VisualIndicator(Color.red)); // Added for Visual Indicators
 
         if(health <= 0){
@@ -81,26 +50,11 @@ public class BotHealth : MonoBehaviour
 
 
     }
-
-    // public void Heal(int amount){
-    //     if(amount <0){
-    //         throw new System.ArgumentOutOfRangeException("Cannot have negative healing");
-    //     }
-
-    //     bool WouldBeOverMAXHEALTH = health + amount > MAX_HEALTH;
-    //     StartCoroutine(VisualIndicator(Color.green)); // Added for Visual Indicators
-
-    //     if(WouldBeOverMAXHEALTH){
-    //         this.health = MAX_HEALTH;
-    //     }else{
-    //         this.health += amount;
-    //     }
-
-        
     // }
 
     private void Die(){
         Debug.Log("DEAD!!");
+        Health.scoreValue += 10;
         Destroy(gameObject);
     }
 }
